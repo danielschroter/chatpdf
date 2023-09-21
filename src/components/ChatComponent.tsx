4cc769462095a483b966b6855a11008cfc56d58c
Daniel Schroter <daniel@schroter.biz>
2023-09-21 14:20:47 +0200

    Chat with document

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -6,15 +6,33 @@ import { Send } from "lucide-react";
 import { Button } from "./ui/button";
 import MessageList from "./MessageList";
 
-type Props = {};
+type Props = {
+  chatId: number;
+};
 
-const ChatComponent = (props: Props) => {
+const ChatComponent = ({ chatId }: Props) => {
   const { input, handleInputChange, handleSubmit, messages } = useChat({
     api: "/api/chat",
+    body: {
+      chatId,
+    },
   });
 
+  React.useEffect(() => {
+    const messageContainer = document.getElementById("message-container");
+    if (messageContainer) {
+      messageContainer.scrollTo({
+        top: messageContainer.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [messages]);
+
   return (
-    <div className="relative h-screen overflow-scroll bg-white ">
+    <div
+      className="relative max-h-screen overflow-scroll bg-white "
+      id="message-container"
+    >
       {/* header */}
       <div className="sticky top-0 inset-x-0 p-2 bg-white h-fit">
         <h3 className="text-xl font-bold">Chat</h3>
