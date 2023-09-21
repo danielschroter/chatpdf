e3d6708f9d7ac3915398615621d5f9125067ce9a
Daniel Schroter <daniel@schroter.biz>
2023-09-21 11:33:06 +0200

    Chat Sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,8 +27,6 @@ export default async function Home() {
             answer questions and unerstand research with AI
           </p>
 
-          {isAuth && <span>{userId}</span>}
-
           <div className="w-full mt-4">
             {isAuth ? (
               <FileUpload></FileUpload>
